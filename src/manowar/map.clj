e62f4a0226b33694d85d7c93d3514e7edc3c2fb1
pce1991 <p_c_eye@yahoo.com>
2014-08-10 15:28:20 -0500

    fleshed out ship a bit

--- src/manowar/map.clj
+++ src/manowar/map.clj
@@ -1,7 +1,10 @@
 (ns manowar.map)
 
+
+;;; To make this work in higher-noon engine just use these as keys and map it to coordinates on screen
 (defn clean-map [radius]
   (reduce concat  (for [i (map inc  (range radius))]
                     (for [j (map inc (range (* radius i)))]
                       [i j]))))
 
+;;; generate a map with opstacles such as asteroids, ion-nebulas, and planets/stars
--- src/manowar/ships.clj
+++ src/manowar/ships.clj
@@ -3,7 +3,13 @@
 (def location-health 25)
 (def charge-drain 5)
 
-(def make-ship  
+(def weapons {:nuke nil 
+              :lasers nil 
+              :flak nil 
+              :gauss nil
+              :missiles nil})
+
+(def ship  
   {:name nil 
    :captain nil 
    :hull 100
@@ -11,24 +17,47 @@
    :power 100                           ;this is calculated by (- max drain) in :generator
    :speed 0
    :course nil 
-   ;; radomize the location of systems. Will take a key matching those in locations. Add more
-   :systems {:computer nil 
+   :location nil 
+   ;; radomize the location of systems. Will take a key matching those in locations. Add more. 
+   ;; there will be between 5 and 8 weapons, so have areas for those as well. 
+   :systems {
+             :computer nil              ;how to balance this? 
              :engine nil
              :generator nil
              :shield nil
-             :scanner nil }
+             :scanner nil
+             :drone-bay nil}
    ;; include more locations
    :locations {:bow location-health
                :stern location-health
                :port location-health
                :starboard location-health
-               :aft location-health}
-   :scanner nil ; a read out of the map, so what the player sees and what the state is are separate.
+               :aft location-health
+               :forward location-health
+               :waist location-health}
+
    :generator {:max-power 100
                :charging nil ;if its charging this will be a number representing how long its been. 
-               :drain 0}
+               :drain 0 ;this increases from sustaining shields, speed, and charging weapons.
+               }
    :computer {:active true
               :access nil
-              :hacked nil}})
+              :hacked nil
+              :scan nil ; a read out of the map, so what the player sees and what the state is are separate.
+              :readout nil ;this is a readout of all systems, and may be incorrect if hacked. 
+              }
+              
+
+   :drones {:deployed nil ;how many are deployed?
+            :mode nil ;this will be recon, assault, or defense
+            :position nil
+            
+            }})
+
+(def drone {:hull 100
+            :location nil
+            :course nil})
+
+(defn make-ship [])
 
 ;;; Need to figure out path-finding if the course is imprecise. 
